28b64f8ec1d306b69fbc47372858bc2139e02082
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-02-09 19:20:50 +0300

    Project1

--- XamarinProject/XamarinProject/Project1.xaml.cs
+++ XamarinProject/XamarinProject/Project1.xaml.cs
@@ -10,16 +10,11 @@ using Xamarin.Forms.Xaml;
 namespace XamarinProject
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Signup : ContentPage
+    public partial class Project1 : TabbedPage
     {
-        public Signup()
+        public Project1()
         {
             InitializeComponent();
-        }
-
-        private async void SignIntn_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new Signup());
         }
     }
 }
