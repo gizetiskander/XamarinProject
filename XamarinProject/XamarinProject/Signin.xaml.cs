be15781e3a38098adca7956f0b35ba464a603443
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-02-09 19:13:26 +0300

    Добавьте файлы проекта.

---
+++ XamarinProject/XamarinProject/Signin.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Signin : ContentPage
+    {
+        public Signin()
+        {
+            InitializeComponent();
+        }
+
+        private async void SignUp_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Signup());
+        }
+
+        private async void SignIn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Projects());
+        }
+    }
+}
